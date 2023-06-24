b619ac66d90a081acf7343026a199cbab8f616de
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-24 15:49:56 +0600

    mongodb connected

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 app.use(express.json());
 app.use(cors());
+
+//MongoDB connection
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at port ${port}`);
 });
