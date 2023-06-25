08f0f99043fe26ba94a513dcdb792bda84a33ab3
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-25 16:21:38 +0600

    deletePost and updatePost

--- controllers/postController.js
+++ controllers/postController.js
@@ -24,6 +24,34 @@ const getAllPosts = async (req, res) => {
   }
 };
 
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
 const likeUnlikePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -45,4 +73,6 @@ module.exports = {
   createPost,
   getAllPosts,
   likeUnlikePost,
+  deletePost,
+  updatePost,
 };
--- routes/postRoute.js
+++ routes/postRoute.js
@@ -2,6 +2,8 @@ const {
   createPost,
   getAllPosts,
   likeUnlikePost,
+  deletePost,
+  updatePost,
 } = require("../controllers/postController");
 
 const router = require("express").Router();
@@ -9,5 +11,7 @@ const router = require("express").Router();
 router.route("/").post(createPost).get(getAllPosts);
 
 router.route("/:id/like").put(likeUnlikePost);
+
+router.route("/:id").delete(deletePost).put(updatePost);
 
 module.exports = router;
