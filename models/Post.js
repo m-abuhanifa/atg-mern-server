db9dcbee0d2f13f0f239db2841e9406aa73557e3
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-28 03:27:33 +0600

    Post model updated

--- models/Post.js
+++ models/Post.js
@@ -1,41 +1,46 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  likes: {
-    type: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    default: [],
-  },
-  comments: {
-    type: [
-      {
-        text: String,
-        user: {
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: {
+      type: [
+        {
           type: mongoose.Schema.Types.ObjectId,
           ref: "User",
         },
-      },
-    ],
-    default: [],
+      ],
+      default: [],
+    },
+    comments: {
+      type: [
+        {
+          text: String,
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+          },
+        },
+      ],
+      default: [],
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Post = mongoose.model("Post", postSchema);
 
